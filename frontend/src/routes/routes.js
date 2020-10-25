d3ab3a3668bfa290ca39654ce4bec2b769202a23
namsefroehlich <66533679+namsefroehlich@users.noreply.github.com>
2020-10-25 16:24:38 +0100

    React Routing

---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import App from "../App";
+
+const Routes = () => {
+    return (
+
+        <Router>
+            <Switch>
+
+            <Route exact path={"/app"} component={App} />
+
+            </Switch>
+        </Router>
+    );
+}
+
+  export default Routes;
