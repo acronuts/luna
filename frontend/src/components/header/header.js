2cb73e73debd728496ec4bd7e250223c01f7b03f
Timon Ritzi <timon.pascal@gmail.com>
2020-10-26 09:38:39 +0100

    login/register/header/footer

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,33 @@
+import logo from "../../assets/svgs/logo.svg"
+
+
+const Header = () => {
+
+    return (
+
+        <div className="header">
+
+                <div className="logo">
+
+                    <img src={logo} id="luna-logo"/>
+
+                </div>
+
+                <div className="home-search-profile">
+
+                    <p>Home</p>
+                    <p>Search</p>
+                    <p>Profile</p>
+
+                </div>
+
+                <div className="signup-login">
+
+                    <p>SIGNUP</p>
+                    <p>LOGIN</p>
+
+                </div>
+
+        </div>
+    )
+}
